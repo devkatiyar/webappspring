2633d88a75f29e9f676cc28fdda973b5d861c82a
devendrakatiyar@gmail.com <devendrakatiyar@gmail.com@303056f6-88ef-227b-b335-3c0df6bfc879>
2013-02-12 09:35:16 +0000

    change the spring security login procedure

--- trunk/springwebapp/src/com/springwebapp/login/dao/UserDaoImpl.java
+++ trunk/springwebapp/src/com/springwebapp/login/dao/UserDaoImpl.java
@@ -1,15 +1,15 @@
 package com.springwebapp.login.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.springwebapp.login.vo.Authority;
 import com.springwebapp.login.vo.User;
 
 
@@ -44,4 +44,23 @@ public class UserDaoImpl implements UserDao {
 		}
 	}
 
+	@Override
+	public List<String> getAuthoritiesByUserName(String userName) {
+		
+		Session session = sessionFactory.getCurrentSession();
+		Criteria criteria = session.createCriteria(User.class);
+		criteria.add(Restrictions.eq("username", userName));
+		List<User> user = criteria.list();
+		System.out.println("loaduserbyname dao" + user);
+		if (user.size() == 1) {
+			Authority a = user.get(0).getAuthority();
+			String auth = a.getAuthority();
+			List<String> l = new ArrayList<String>();
+			l.add(auth);
+			return l;
+		} else {
+			return null;
+		}
+
+	}
 }
--- trunk/springwebapp/src/com/springwebapp/login/services/UserUserDetailsService.java
+++ trunk/springwebapp/src/com/springwebapp/login/services/UserUserDetailsService.java
@@ -1,35 +1,40 @@
 package com.springwebapp.login.services;
 
+import java.util.Collection;
+import java.util.List;
+
 import org.springframework.dao.DataAccessException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.springwebapp.login.services.UserService;
 import com.springwebapp.login.dao.UserDao;
 import com.springwebapp.login.vo.User;
 
 public class UserUserDetailsService implements UserDetailsService {
-
-	private UserDao userDao;
-
-	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException, DataAccessException {
+	private UserService userService;
+	public UserUserDetailsService(UserService userService) {
+		this.userService = userService;
+	}
+	
+	public UserDetails loadUserByUsername(String userName)
+			throws UsernameNotFoundException, DataAccessException {
 		User user;
 		try {
-			user = userDao.getUserByUserName(userName);
-			GrantedAuthority grantedAuthority = new UserGrantedAuthority(user.getRole());
-			CustomUser customUser = new CustomUser(user.getUsername(), user.getPassword(), new GrantedAuthority[] { grantedAuthority });
+			user = userService.getUserByUserName(userName);
+			List<String> authorities = userService.getAuthoritiesByUserName(userName);
+			user.setUserAuthorities(authorities);
+			Collection<GrantedAuthority> grantedAuthority = user.getAuthorities();
+			Object a[] = grantedAuthority.toArray();
+			GrantedAuthority authority = new UserGrantedAuthority(a[0].toString());
+			CustomUser customUser = new CustomUser(user.getUsername(),user.getPassword(), new GrantedAuthority[] { authority });
 			return customUser;
+
 		} catch (Exception e) {
-			throw new UsernameNotFoundException("getUserByUserName returned null.");
+			throw new UsernameNotFoundException(
+					"getUserByUserName returned null.");
 		}
 	}
-
-	public UserDao getUserDao() {
-		return userDao;
-	}
-
-	public void setUserDao(UserDao userDao) {
-		this.userDao = userDao;
-	}
-
 }
